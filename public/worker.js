e2d5b3b74847924fccf84da86d09112eaef58acb
overhead525 <ritchelcousar@outlook.com>
2021-05-12 14:19:57 -0400

    created a worker for handling websocket connection to... coinbase

---
+++ public/worker.js
@@ -0,0 +1,51 @@
+import moment from "moment";
+
+console.log(`From worker: worker started at ${moment().format("HH:mm:ss")}`);
+
+function waitForSocketConnection(socket, callback) {
+  setTimeout(function () {
+    if (socket.readyState === 1) {
+      console.log("Connection is made");
+      if (callback != null) {
+        callback();
+      }
+    } else {
+      console.log("wait for connection...");
+      waitForSocketConnection(socket, callback);
+    }
+  }, 5); // wait 5 milisecond for the connection...
+}
+
+function sendMessage(msg) {
+  // Wait until the state of the socket is not ready and send the message when it is...
+  waitForSocketConnection(coinbaseSocket, function () {
+    console.log("message sent!!!");
+    coinbaseSocket.send(msg);
+  });
+}
+
+const coinbaseSocket = new WebSocket("wss://ws-feed.pro.coinbase.com");
+
+const toSend = {
+  type: "subscribe",
+  channels: [
+    {
+      name: "ticker",
+      product_ids: ["ETH-USD"],
+    },
+  ],
+};
+
+sendMessage(JSON.stringify(toSend));
+
+coinbaseSocket.onmessage = function (e) {
+  const price = parseFloat(JSON.parse(e.data).price);
+  if (price) postMessage(JSON.stringify({ price }));
+};
+
+onmessage = function (e) {
+  console.log("Worker: Message received from main script");
+  this.postMessage("sent back from worker");
+};
+
+export default self;

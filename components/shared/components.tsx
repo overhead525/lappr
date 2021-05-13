f805dea5fb9eb1634434cd1afca8f3a3a2a30275
overhead525 <ritchelcousar@outlook.com>
2021-05-13 19:47:21 -0400

    create shared folder for re-used components

---
+++ components/shared/components.tsx
@@ -0,0 +1,11 @@
+import {
+  BitcoinSymbolDisplay,
+  EthereumSymbolDisplay,
+  StellarLumenSymbolDisplay,
+} from "../../stories/orderPage/SymbolDisplay.stories";
+
+export const symbolDisplays = {
+  "BTC-USD": <BitcoinSymbolDisplay />,
+  "ETH-USD": <EthereumSymbolDisplay />,
+  "XLM-USD": <StellarLumenSymbolDisplay />,
+};
---
+++ components/shared/layout.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledScreenWrapper = styled.div`
+  max-width: 1000px;
+  background-color: #121212;
+  padding: 0 3vw;
+`;

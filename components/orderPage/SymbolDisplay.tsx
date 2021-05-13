54c22bbd5799cbf8497b82a23c52f0cc643f5416
overhead525 <ritchelcousar@outlook.com>
2021-05-13 15:17:10 -0400

    changed background color to match dark theme

--- components/orderPage/SymbolDisplay.tsx
+++ components/orderPage/SymbolDisplay.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 export interface SymbolDisplayProps {
   cryptoLogoURL?: string;
   symbol: string;
+  invert: boolean;
 }
 
 const StyledSymbolDisplay = styled.div`
@@ -13,21 +14,24 @@ const StyledSymbolDisplay = styled.div`
   width: 43.8vw;
   height: 43.8vw;
 
+  background-color: #121212;
   border-radius: 2vw;
   box-shadow: 0.9vw 0.9vw 2vw #000000, -0.9vw -0.9vw 2vw #202324;
 `;
 
 const StyledImage = styled.img`
   width: 26.8vw;
+  filter: invert(${(props) => (props.secondary ? 1 : 0)});
 `;
 
 export const SymbolDisplay: React.FC<SymbolDisplayProps> = ({
   cryptoLogoURL,
   symbol,
+  invert,
 }) => {
   return (
     <StyledSymbolDisplay>
-      <StyledImage src={cryptoLogoURL} alt="Bitcoin Logo" />
+      <StyledImage src={cryptoLogoURL} alt="Bitcoin Logo" secondary={invert} />
       {/*<Image
         src={}
         alt={symbol}
